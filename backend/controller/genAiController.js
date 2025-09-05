2fd821c977bacaec694655283b83e5afa22299ac
officialshivangik11@gmail.com <officialshivangik11@gmail.com>
2025-09-06 00:19:36 +0530

    implementing ai api at backend tested

--- backend/controller/genAiController.js
+++ backend/controller/genAiController.js
@@ -1,10 +1,12 @@
-import * as ai from "../services/genAiServices.js"
+// import * as ai from "../services/genAiServices.js"
+import main from "../services/genAiServices.js"
 
 
 export  const getResult=async(req,res)=>{
     try{
         const {prompt}=req.query
-        const result=await ai.generateResult(prompt);
+        // const result=await ai.generateResult(prompt);
+        const result=await main(prompt);
         res.send (result);
     }catch(error){
         res.status(500).send({message:error.message})
--- backend/services/genAiServices.js
+++ backend/services/genAiServices.js
@@ -1,60 +1,15 @@
 import { GoogleGenAI } from "@google/genai";
 
-const ai = new GoogleGenAI(process.env.GOOGLE_AI_KEY);
+const ai = new GoogleGenAI(process.env.GOOGLE_API_KEY);
 
 async function main(prompt) {
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
     contents: prompt,
   });
+  // console.log(response.text);
+  
 return response.text;
 }
 
-// await main();
 export default main;
-
-// import { GoogleGeneAI } from "@google/genai";
-
-// const genAI = new GoogleGeneAI(process.env.GOOGLE_AI_KEY);
-
-// async function generateResult(prompt) {
-//   // get the model
-//   const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
-
-//   // generate response
-//   const result = await model.generateContent(prompt);
-
-//   // return plain text
-//   return result.response.text();
-// }
-
-// export default generateResult;
-
-
-
-// import { GoogleGenAI } from "@google/genai";
-
-// const genAI = new GoogleGenAI(process.env.GOOGLE_AI_KEY);
-
-// async function generateResult(prompt) {
-//   const response = await genAI.models.generateContent({
-//     model: "gemini-2.5-flash",
-//     contents: prompt,
-//   });
-//   return response.text;
-// }
-
-// export default generateResult;
-
-// import { GoogleGenAI } from "@google/genai";
-
-// const genAI = new GoogleGenAI(process.env.GOOGLE_AI_KEY);
-// const model=genAI.getGen
-// export const generateResult=async(prompt)=>{
-//   const prompt="Explain how AI works in a few words";
-//   const result=await model.generateContent(prompt)
-//   return result.response.text();
-  
-  
-// }
-
