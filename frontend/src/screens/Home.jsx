df2ecd9c2f2d25496fa4dcb0e6fb3da36ce9da39
officialshivangik11@gmail.com <officialshivangik11@gmail.com>
2025-09-26 16:22:34 +0530

    styling home page ui

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,9 +1,11 @@
-import React, { useContext, useState, useEffect } from 'react'
+import React, { useContext, useState, useEffect, useRef } from 'react'
 import axios from "../config/axios"
 import { useNavigate } from 'react-router-dom'
 import { UserContext } from '../context/userContext'
 import Navbar from "./Navbar"
-import sideimg from "../assets/download.png"
+import bgvideo from "../assets/Chatbg.mp4"
+import { gsap } from 'gsap';
+
 const Home = () => {
 
     const { user } = useContext(UserContext)
@@ -12,6 +14,17 @@ const Home = () => {
     const [ project, setProject ] = useState([])
 
     const navigate = useNavigate()
+    
+const headingRef = useRef(null);
+
+useEffect(() => {
+  gsap.from(headingRef.current, {
+    opacity: 0,
+    y: -30,
+    duration: 1,
+    ease: 'power3.out',
+  });
+}, []);
 
     function createProject(e) {
         e.preventDefault()
@@ -23,6 +36,10 @@ const Home = () => {
             .then((res) => {
                 console.log(res)
                 setIsModalOpen(false)
+                // navigate("/project")
+                navigate("/project", {
+            state: { project }  // <-- backend should return created project
+          });
             })
             .catch((error) => {
                 console.log(error)
@@ -41,29 +58,40 @@ const Home = () => {
     }, [])
 
     return (<>
-        {/* <Navbar/> */}
-        <main className='flex'>
-            <div className='flex gap-10  bg-gray-800 '>
-                
-                      <div className="projects h-auto flex flex-wrap gap-2  bg-gray-800 p-10 xl:p-40">
+        <main className='flex h-max-auto bg-black  ' >
+            {/* <div className='flex gap-10  bg-gray-800 '> */}
+          <div className="relative  w-full  z-10 min-h-screen flex flex-col justify-center items-center text bg-black">
+  <video
+    className="absolute top-0 left-0 w-full h-full object-cover bg-black bg-opacity-40 -z-10"
+    autoPlay
+    muted
+    loop
+  >
+    <source src={bgvideo} type="video/mp4" />
+  </video>
+{/* </div>   */}
+
+      <div className="projects h-auto flex flex-wrap gap-2  bg-transparent backdrop-brightness-100 p-10 xl:p-30 z-40">
+        <Navbar/>
                 <div className='flex gap-2 flex-col'>
-                    {/* <h1 className='text-8xl text-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'> Build Meaningful Connection and elevate  your Career</h1> */}
-                    {/* <h1 className='text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 p-4'> Build Meaningful Connection and elevate  your Career</h1> */}
-                    <h1 className='text-6xl xl:text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-bl from-[#e879f9] via-[#4ade80] to-[#be123c] p-4'> Build Meaningful Connection and elevate  your Career</h1>
-                    <p className='text-gray-300 text-lg p-6 xl:text-2xl'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae amet officiis maiores quia odit accusantium, modi eius repudiandae natus, architecto at neque, magni velit suscipit laboriosam labore atque cumque alias.</p>
+
+                    <h1 className='text-6xl xl:text-9xl font-extrabold text-transparent xl:pr-50 bg-clip-text bg-gradient-to-bl from-[#e879f9] via-[#4ade80] to-[#be123c] p-5'> 
+                        Build Meaningful Connection and elevate  your Career</h1>
+                    {/* <p className='text-gray-300 text-lg p-6 xl:text-2xl'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae amet officiis maiores quia odit accusantium, modi eius repudiandae natus, architecto at neque, magni velit suscipit laboriosam labore atque cumque alias.</p> */}
                         </div>                
                   <div className='flex flex-col gap-10'>
                     <div>
                          <button
                     onClick={() => setIsModalOpen(true)}
-                    className="project p-4 border lg:text-xl font-mono text-gray-50 border-slate-300 rounded-md ">
+                    className="project p-4 border lg:text-xl mt-3   bg-green-950 cursor-pointer text-center font-mono text-gray-50 border-slate-300 rounded-md  z-20">
                     New Project
                     <i className="ri-link ml-2 text-white"></i>
                      </button>
                     </div>
-                    <div className=' flex flex-col gap-5'>
-                     <div className="text-4xl font-mono text-green-400 py-3 hover:underline">My Projects</div>
-                                {
+                    <div className=' flex flex-col gap-5  '>
+                     <div className="text-4xl flex w-full justify-center items-center font-mono text-green-400 py-3 hover:underline text-center">My Projects</div>
+                   <div className=' grid grid-cols-1  gap-4 xl:grid-cols-4  xl:p-6 xl:gap-8 '>
+                                 {
                     project.map((project) => (
                         <div key={project._id}
                             onClick={() => {
@@ -71,12 +99,12 @@ const Home = () => {
                                     state: { project }
                                 })
                             }}
-                            className="project flex-col gap-2 cursor-pointer p-2 border border-slate-300 rounded-md min-w-auto hover:bg-gradient-to-t from-[#e879f9] via-[#4ade80] to-[#da6381]">
+                            className="project flex-col xl:p-6 gap-2 cursor-pointer p-2 border bg-[#303030] border-slate-300 rounded-md min-w-auto hover:opacity-70 ">
                             <h2
                                 className='font-semibold text-white'
                             >{project.name}</h2>
 
-                            <div className="flex gap-2 text-gray-500 text-lg hover:text-black">
+                            <div className="flex gap-2 text-gray-500 text-lg">
                                 <p> <small> <i className="ri-user-line"></i> Collaborators</small> :</p>
                                 {project.users.length}
                             </div>
@@ -84,19 +112,20 @@ const Home = () => {
                         </div>
                     ))
                 }
+                   </div>
                    </div>   
                    </div>   
 
 
 
             </div>
-            <div className=' p-20' ><img src={sideimg} alt="" /></div>
+            {/* <div className=' p-20' ><img src={sideimg} alt="" /></div> */}
             </div>
       
 
             {isModalOpen && (
-                <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
-                    <div className="bg-gray-300 p-6 rounded-md shadow-md w-1/3">
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+                    <div className="bg-gray-300 p-6  rounded-md shadow-md w-1/2 xl:w-1/3">
                         <h2 className="text-2xl mb-4">Create New Project</h2>
                         <form onSubmit={createProject}>
                             <div className="mb-4">
@@ -109,7 +138,7 @@ const Home = () => {
                             </div>
                             <div className="flex justify-end">
                                 <button type="button" className="mr-2 px-4 py-2 bg-gray-300 rounded-md" onClick={() => setIsModalOpen(false)}>Cancel</button>
-                                <button type="submit" className="px-4 py-2 bg-blue-600 text-white rounded-md">Create</button>
+                                <button type="submit" className="px-4 py-2 bg-[#37a35f] text-white rounded-md">Create</button>
                             </div>
                         </form>
                     </div>
--- frontend/src/screens/Navbar.jsx
+++ frontend/src/screens/Navbar.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-
+import logo from "../assets/chatlogo.png"
 function Navbar() {
   return (
-    <div>
-        <img src="" alt="" />
-        <h1>navbar</h1>
+    <div className=' w-full flex flex-wrap bg-transparent z-10  '>
+        <img src={logo}  className='h-18' alt="" />
     </div>
   )
 }
